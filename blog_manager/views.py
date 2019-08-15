3339d04f15c5370d58c4e9940309bb9fc56f7e4b
Jung <wjddms04@hanyang.ac.kr>
2019-08-16 02:42:05 +0900

    그룹장 창에서 각 기능 들어가는 링크 연결

--- blog_manager/views.py
+++ blog_manager/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,7 +12,31 @@ def group_manage(request, pk):
     members = Profile.objects.filter(group=group)
 
     ctx = {
+        'pk':pk,
         'group': group,
         'members': members,
     }
+
     return render(request, 'blog_manager/group_manage.html', ctx)
+
+
+def group_info_update(request, pk):
+    return HttpResponse('그룹 업데이트를 해줄 친절한 누군가를 찾아요')
+
+def group_member_manage(request, pk):
+    ctx = {
+        'pk': pk,
+    }
+    return render(request, 'blog_manager/manage_group_member.html', ctx)
+
+def invite_member(request, pk):
+    ctx = {
+        'pk': pk,
+    }
+    return render(request, 'blog_manager/invite_member.html', ctx)
+
+def manage_requests(request, pk):
+    ctx = {
+        'pk': pk,
+    }
+    return render(request, 'blog_manager/manage_requests.html', ctx)
