52e7ee252e7594e455af7691fe28219556d1ef49
Jung <wjddms04@hanyang.ac.kr>
2019-08-16 01:07:23 +0900

    그룹장페이지-그룹 기본정보 보기 구현

--- blog_manager/views.py
+++ blog_manager/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from users.models import Group, Profile, GroupMember
 
 
 def group_manage(request, pk):
-    return render(request, 'blog_manager/group_manage.html')
+    profile = Profile.objects.get(user=request.user)
+
+    group = Group.objects.get(id=pk)
+    members = Profile.objects.filter(group=group)
+
+    ctx = {
+        'group': group,
+        'members': members,
+    }
+    return render(request, 'blog_manager/group_manage.html', ctx)
